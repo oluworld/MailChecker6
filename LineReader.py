d22b6ede2b43d33ef744ca5bd8a98ba6128b6caa
oluworld <oluoluolu@protonmail.com>
2001-11-28 23:12:48 -0500

    LineReader.py

---
+++ LineReader.py
@@ -0,0 +1,17 @@
+class LineReader:
+	def __init__ (self, src):
+		self.lines = src
+		self.pos = 0
+		self.name = '?'
+	def __del__ (self):
+		self.close ()
+	def readline (self):
+		try:
+			r = self.lines[self.pos] #+'\n'
+			self.pos = self.pos + 1
+		except IndexError:
+			r = None
+		return r
+	def close (self):
+		#self.lines = None
+		del self.lines
